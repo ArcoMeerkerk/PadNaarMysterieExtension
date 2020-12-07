45a1c88d36422574b41f6eb8b369d1c366a544b4
DraxNoid <jjma2003@gmail.com>
2020-12-07 20:18:08 +0100

    De vecht knop drukbaar gemaakt.

--- PadNaarMysterieExtension.pyde
+++ PadNaarMysterieExtension.pyde
@@ -1,15 +1,9 @@
-#Width, height
-appSize = [800, 600]
 from modules import vechtknop
-
 def setup():
     # The size of the canvas
-    size(appSize[0], appSize[1])
+    size(800, 600)
+    # Zet Achtergrond zwart
     background(0)
-    # dice.setupFuctions()
-
 def draw():
     vechtknop.knop()
-
-# def keyPressed():
-# def keyReleased():
+    print(mouseX, mouseY)
--- modules/vechtknop.py
+++ modules/vechtknop.py
@@ -1,7 +1,16 @@
+gedrukt = False
 def knop():
-    fill(255)
-    rect(300, 500, 200, 50)
-    fill(0)
-    textSize(32)
-    textAlign(CENTER)
-    text("VECHT", 400,540)
+    global gedrukt
+    if gedrukt == False:
+        # Rechthoek
+        fill(255)
+        rect(300, 500, 200, 50)
+        # Vecht
+        fill(0)
+        textSize(32)
+        textAlign(CENTER)
+        text("VECHT", 400, 540)
+        if mousePressed:
+            if (mouseX > 300 and mouseX < (300 + 200) and mouseY > 500 and mouseY < (500 + 50)):
+                background(0)
+                gedrukt = True

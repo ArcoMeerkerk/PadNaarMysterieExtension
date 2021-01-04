d172deeb65cb770a498918fc2124e0b7798b546e
Rizkies <73880174+Rizkies@users.noreply.github.com>
2021-01-04 11:30:52 +0100

    latest

--- .vscode/dice/dice.py
+++ .vscode/dice/dice.py
@@ -1,9 +1,16 @@
 import random
 
-def Draw():
+def setup():
     size(800,600)
     background(255)
+    ButtonRightDice()
+    ButtonLeftDice()
 
+def upperText():
+    fill(0)
+    textSize(50)
+    textAlign(CENTER)
+    text('')
 def rollRightDice(): 
     rolleddice = int(random(1, 7))
     if rolleddice == 1:
@@ -38,7 +45,8 @@ def rollRightDice():
         imageMode(CENTER)
 
 def rollLeftDice(): 
-    rolleddice = int(random(1, 13))
+    rolleddice = int(random(1, 7))
+    rolleddice1 = int(random(1, 7))
     if rolleddice == 1:
         fill(0)
         global img1
@@ -69,33 +77,43 @@ def rollLeftDice():
         global img6
         img6 = loadImage("dice6.png")
         imageMode(CENTER)
-    if rolleddice == 7:
+    if rolleddice1 == 1:
         fill(0)
         global img1
-        img1 = loadImage("dice7.png")
+        img1 = loadImage("dice1.png")
         imageMode(CENTER)
-    if rolleddice == 8:
+    if rolleddice1 == 2:
         fill(0)
         global img2
-        img2 = loadImage("dice8.png")
+        img2 = loadImage("dice2.png")
         imageMode(CENTER)
-    if rolleddice == 9:
+    if rolleddice1 == 3:
         fill(0)
         global img3
-        img3 = loadImage("dice9.png")
+        img3 = loadImage("dice3.png")
         imageMode(CENTER)
-    if rolleddice == 10:
+    if rolleddice1 == 4:
         fill(0)
         global img4
-        img4 = loadImage("dice10.png")
+        img4 = loadImage("dice4.png")
         imageMode(CENTER)
-    if rolleddice == 11:
+    if rolleddice1 == 5:
         fill(0)
         global img5
-        img5 = loadImage("dice11.png")
+        img5 = loadImage("dice5.png")
         imageMode(CENTER)
-    if rolleddice == 12:
+    if rolleddice1 == 6:
         fill(0)
         global img6
-        img6 = loadImage("dice12.png")
+        img6 = loadImage("dice6.png")
         imageMode(CENTER)
+
+def ButtonLeftDice():
+    fill(0)
+    rectMode(CORNER)
+    rect(100, 500, 120, 50)
+
+def ButtonRightDice():
+    fill(0)
+    rectMode(CORNER)
+    rect(600, 500, 120, 50)

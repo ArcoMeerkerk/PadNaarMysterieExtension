a6008d36c8a035f417c6aa67808893d20f72decd
dfenders2002 <58844581+dfenders2002@users.noreply.github.com>
2021-01-04 15:55:04 +0100

    Fixed event timer

--- modules/Event.py
+++ modules/Event.py
@@ -3,44 +3,62 @@
 import json
 import random
 #import PIL
-#backgroundPicture = loadImage("Bord-3.png")
 Gamemode = "Start"
 #json file inladen + Eventrunner
 myjsonfile = open('../assets/json/Evenementen.json', 'r')
 jsondata = myjsonfile.read()
 Eventrunner = True
 obj = json.loads(jsondata)
-def eventbepaler():
+timer_for_event = 0
+event_time = 600
+timer_enabled = True
+random_event_time = 0
+def loadEvent():
     #kiest een random value/event uit Evenementen.json en return deze
     EventKeuze = (random.choice(list(obj.values())))
     return str(EventKeuze)
 def setup():
     global backgroundPicture, backgroundPicture
     # The size of the canvas + foto inladen + tekst centreren
-    # backgroundPicture = loadImage("Bord-3.png")
-    backgroundPicture = None
+    backgroundPicture = loadImage("../assets/images/Bord-0.png")
     size(800,600)
     textAlign(CENTER,CENTER)
-def event():
+def ShowEvent():
     global Gamemode, EventKeuze, Eventrunner, backgroundPicture
     background(100)
     #Afbeelding inladen
-    #image(backgroundPicture, 150, 100, 500, 300)
+    image(backgroundPicture, 150, 100, 500, 300)
     textSize(26)
     #runt eventrunner 1 keer om te bepalen welke event er geprint wordt op de afbeelding
     if Eventrunner == True:
         Eventrunner = False
-        EventKeuze = eventbepaler()
+        EventKeuze = loadEvent()
     text(EventKeuze , 150, 50,500,300)
+    if key == 'o':
+        background(0)
+        return True
+    else:
+        return False
+def timer():
+    global timer_for_event, random_event_time, timer_enabled
+    print("timer_for_event", timer_for_event)
+    print("random_event_time", random_event_time)
+    if timer_for_event == 0 or timer_for_event >= event_time:
+        timer_for_event = 1
+        random_event_time = random.choice(range(event_time))
+    elif timer_for_event < event_time and timer_enabled == True: 
+        timer_for_event += 1
+    if random_event_time >= timer_for_event:
+        timer_enabled = ShowEvent()
+    else:
+        timer_enabled = True
+
 def draw():
-    global Gamemode, Eventrunner
+    global Gamemode, Eventrunner, timer_for_event
     if Gamemode == "Start":
         background(0)
-        if key == 'e': 
-            event()
-        elif key == 'q':
-            background(0)
-            Eventrunner = True
+        timer()
+        
     
 
 # def draw():

edcc7bf465b1e220bc841f10e06ecb3947cbc83c
Rizkies <73880174+Rizkies@users.noreply.github.com>
2021-01-09 13:59:15 +0100

    Update dice.py

--- .vscode/dice/dice.py
+++ .vscode/dice/dice.py
@@ -14,7 +14,6 @@ def setup():
     size(800,600)
 
 def draw():
-    background(255)
     RollButtons()
     upperText()
 
@@ -23,147 +22,60 @@ def upperText():
         fill(0)
         textSize(50)
         textAlign(CENTER)
-        text('Choose Right or Left dice', width/2, 175)
-        textSize(25)
-        text('The right one has 6 sides and the left one has 12 sides', width/2, 275)
+        text('Click the dice', width/2, 175)
 
 def rollRightDice(): 
-    global img1, img2, img3, img4, img5, img6, false, y
+    global img1, img2, img3, img4, img5, img6, false, gedrukt
     rolleddice = int(random.randint(1, 6))
     if gedrukt == False:
-        while y < 1:
-            if rolleddice == 1:
-                background(255)
-                imageMode(CENTER)
-                image(img1, 400, 250)
-                y += 1
-            if rolleddice == 2:
-                background(255)
-                imageMode(CENTER)
-                image(img2, 400, 250)
-                y += 1
-            if rolleddice == 3:
-                background(255)
-                imageMode(CENTER)
-                image(img3, 400, 250)
-                y += 1
-            if rolleddice == 4:
-                background(255)
-                imageMode(CENTER)
-                image(img4, 400, 250)
-                y += 1
-            if rolleddice == 5:
-                background(255)
-                imageMode(CENTER)
-                image(img5, 400, 250)
-                y += 1
-            if rolleddice == 6:
-                background(255)
-                imageMode(CENTER)
-                image(img6, 400, 250)
-                y += 1
-
-def rollLeftDice():
-    global img1, img2, img3, img4, img5, img6, y
-    rolleddice = int(random.randint(1, 6))
-    rolleddice1 = int(random.randint(1, 6))
-    if gedrukt == False:
-        while y < 1:
-            if rolleddice == 1:
-                background(255)
-                imageMode(CENTER)
-                image(img1, 250, 150, 100, 100)
-                y += 1
-            if rolleddice == 2:
-                background(255)
-                imageMode(CENTER)
-                image(img2, 250, 150, 100, 100)
-                y += 1
-            if rolleddice == 3:
-                background(255)
-                imageMode(CENTER)
-                image(img3, 250, 150, 100, 100)
-                y += 1
-            if rolleddice == 4:
-                background(255)
-                imageMode(CENTER)
-                image(img4, 250, 150, 100, 100)
-                y += 1
-            if rolleddice == 5:
-                background(255)
-                imageMode(CENTER)
-                image(img5, 250, 150, 100, 100)
-                y += 1
-            if rolleddice == 6:
-                background(255)
-                imageMode(CENTER)
-                image(img6, 250, 150, 100, 100)
-                y += 1
-            if rolleddice1 == 1:
-                background(255)
-                imageMode(CENTER)
-                image(img1, 450, 150, 100, 100)
-                y += 1
-            if rolleddice1 == 2:
-                background(255)
-                imageMode(CENTER)
-                image(img2, 450, 150, 100, 100)
-                y += 1
-            if rolleddice1 == 3:
-                background(255)
-                imageMode(CENTER)
-                image(img3, 450, 150, 100, 100)
-                y += 1
-            if rolleddice1 == 4:
-                background(255)
-                imageMode(CENTER)
-                image(img4, 450, 150, 100, 100)
-                y += 1
-            if rolleddice1 == 5:
-                background(255)
-                imageMode(CENTER)
-                image(img5, 450, 150, 100, 100)
-                y += 1
-            if rolleddice1 == 6:
-                background(255)
-                imageMode(CENTER)
-                image(img6, 450, 150, 100, 100)
-                y += 1
+        if rolleddice == 1:
+            background(255)
+            imageMode(CENTER)
+            image(img1, 401, 233, 227, 227)
+        if rolleddice == 2:
+            background(255)
+            imageMode(CENTER)
+            image(img2, 401, 233, 227, 227)
+        if rolleddice == 3:
+            background(255)
+            imageMode(CENTER)
+            image(img3, 401, 233, 227, 227)
+        if rolleddice == 4:
+            background(255)
+            imageMode(CENTER)
+            image(img4, 401, 233, 227, 227)
+        if rolleddice == 5:
+            background(255)
+            imageMode(CENTER)
+            image(img5, 401, 233, 227, 227)
+        if rolleddice == 6:
+            background(255)
+            imageMode(CENTER)
+            image(img6, 401, 233, 227, 227)
+    if gedrukt == True:
+        fill(255)
+        rectMode(CORNER)
+        rect(278, 472, 243, 64)
+        textAlign(CENTER, CENTER)
+        fill(0)
+        textSize(20)
+        text('Terug naar homescreen', 400, 498)
+        if (mouseX > 278 and mouseX < (278 + 243) and mouseY > 472 and mouseY < (472 + 64)):      
+            rolleddice = 0
+            background(255)
+            gedrukt = False              
 
 def RollButtons():
     global img3, img6, gedrukt
     if gedrukt == False:
         fill(255)
         rectMode(CORNER)
-        rect(580, 450, 120, 120)
+        rect(330, 380, 143, 143)
         imageMode(CORNER)
-        image(img6, 580, 450, 120, 120)
-
-# Left button
-    if gedrukt == False:
-        fill(255)
-        rectMode(CORNER)
-        rect(100, 450, 120, 120)
-        imageMode(CORNER)
-        image(img3, 100, 450, 120, 120)
-
+        image(img6, 330, 380, 143, 143)
 
     if mousePressed:
-        if (mouseX > 100 and mouseX < (100 + 120) and mouseY > 450 and mouseY < (450 + 120)):
-            rollLeftDice()
-            gedrukt = True
-        elif (mouseX > 580 and mouseX < (580 + 120) and mouseY > 450 and mouseY < (450 + 120)):
+        if (mouseX > 330 and mouseX < (330 + 143) and mouseY > 380 and mouseY < (380 + 143)):
             rollRightDice()
             gedrukt = True
-
-# def MouseInSpace(x, y, w, h):
-#     if (mouseX > x) and (mouseX < x+w) and (mouseY > y) and (MouseY < y+h):
-#         return True
-#     else:
-#         return False
-
-# def mousePressed():
-#     if MouseInSpace(100, 450, 250, 250)
-#         rollRightDice()
-#     if MouseInSpace(100, 250, 250, 250)
-#         rollLeftDice()
+    

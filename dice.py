7cbf428df47990c9343d0093411f3f9b511a65c9
Arco Meerkerk <arco-meerkerk@live.nl>
2021-01-12 00:16:43 +0100

    fixed code

--- dice.py
+++ dice.py
@@ -1,127 +1,84 @@
-import random
-
+gedrukt = False
+y = 0
+images = []
 
 def setup():
+    global images
+    
     size(800,600)
-    background(255)
+
+    images = [
+        loadImage("./assets/images/dice1.png"),
+        loadImage("./assets/images/dice2.png"),
+        loadImage("./assets/images/dice3.png"),
+        loadImage("./assets/images/dice4.png"),
+        loadImage("./assets/images/dice5.png"),
+        loadImage("./assets/images/dice6.png"),
+    ]
+
+def draw():
     RollButtons()
     upperText()
 
 def upperText():
-    fill(0)
-    textSize(50)
-    textAlign(CENTER)
-    text('Choose Left or Right dice', width/2, 175)
-    textSize(25)
-    text('The right one has 6 sides and the left one has 12 sides', width/2, 275)
+    if gedrukt == False:
+        fill(0)
+        textSize(50)
+        textAlign(CENTER)
+        text('Click the dice', width/2, 175)
 
 def rollRightDice(): 
-    rolleddice = int(random(1, 7))
-    if rolleddice == 1:
+    global images, false, gedrukt
+    rolleddice = int(random(1, 6))
+    if gedrukt == False:
+        if rolleddice == 1:
+            background(255)
+            imageMode(CENTER)
+            image(images[0], 401, 233, 227, 227)
+        if rolleddice == 2:
+            background(255)
+            imageMode(CENTER)
+            image(images[1], 401, 233, 227, 227)
+        if rolleddice == 3:
+            background(255)
+            imageMode(CENTER)
+            image(images[2], 401, 233, 227, 227)
+        if rolleddice == 4:
+            background(255)
+            imageMode(CENTER)
+            image(images[3], 401, 233, 227, 227)
+        if rolleddice == 5:
+            background(255)
+            imageMode(CENTER)
+            image(images[4], 401, 233, 227, 227)
+        if rolleddice == 6:
+            background(255)
+            imageMode(CENTER)
+            image(images[5], 401, 233, 227, 227)
+    if gedrukt == True:
+        fill(255)
+        rectMode(CORNER)
+        rect(278, 472, 243, 64)
+        textAlign(CENTER, CENTER)
         fill(0)
-        global img1
-        img1 = loadImage("dice1.png")
-        imageMode(CENTER)
-    if rolleddice == 2:
-        fill(0)
-        global img2
-        img2 = loadImage("dice2.png")
-        imageMode(CENTER)
-    if rolleddice == 3:
-        fill(0)
-        global img3
-        img3 = loadImage("dice3.png")
-        imageMode(CENTER)
-    if rolleddice == 4:
-        fill(0)
-        global img4
-        img4 = loadImage("dice4.png")
-        imageMode(CENTER)
-    if rolleddice == 5:
-        fill(0)
-        global img5
-        img5 = loadImage("dice5.png")
-        imageMode(CENTER)
-    if rolleddice == 6:
-        fill(0)
-        global img6
-        img6 = loadImage("dice6.png")
-        imageMode(CENTER)
-
-def rollLeftDice(): 
-    rolleddice = int(random(1, 7))
-    rolleddice1 = int(random(1, 7))
-    if rolleddice == 1:
-        fill(0)
-        global img1
-        img1 = loadImage("dice1.png")
-        imageMode(CENTER)
-    if rolleddice == 2:
-        fill(0)
-        global img2
-        img2 = loadImage("dice2.png")
-        imageMode(CENTER)
-    if rolleddice == 3:
-        fill(0)
-        global img3
-        img3 = loadImage("dice3.png")
-        imageMode(CENTER)
-    if rolleddice == 4:
-        fill(0)
-        global img4
-        img4 = loadImage("dice4.png")
-        imageMode(CENTER)
-    if rolleddice == 5:
-        fill(0)
-        global img5
-        img5 = loadImage("dice5.png")
-        imageMode(CENTER)
-    if rolleddice == 6:
-        fill(0)
-        global img6
-        img6 = loadImage("dice6.png")
-        imageMode(CENTER)
-    if rolleddice1 == 1:
-        fill(0)
-        global img1
-        img1 = loadImage("dice1.png")
-        imageMode(CENTER)
-    if rolleddice1 == 2:
-        fill(0)
-        global img2
-        img2 = loadImage("dice2.png")
-        imageMode(CENTER)
-    if rolleddice1 == 3:
-        fill(0)
-        global img3
-        img3 = loadImage("dice3.png")
-        imageMode(CENTER)
-    if rolleddice1 == 4:
-        fill(0)
-        global img4
-        img4 = loadImage("dice4.png")
-        imageMode(CENTER)
-    if rolleddice1 == 5:
-        fill(0)
-        global img5
-        img5 = loadImage("dice5.png")
-        imageMode(CENTER)
-    if rolleddice1 == 6:
-        fill(0)
-        global img6
-        img6 = loadImage("dice6.png")
-        imageMode(CENTER)
+        textSize(20)
+        text('Terug naar homescreen', 400, 498)
+        if (mouseX > 278 and mouseX < (278 + 243) and mouseY > 472 and mouseY < (472 + 64)):      
+            rolleddice = 0
+            background(255)
+            gedrukt = False              
 
 def RollButtons():
-    fill(255)
-    rectMode(CORNER)
-    rect(100, 500, 120, 120)
-    global img6
-    img6 = loadImage('../images/dice6.png')
-    imageMode(CORNER)
-    image(img6, 100, 500, 120, 120)
+    global images,gedrukt
+    if gedrukt == False:
+        fill(255)
+        rectMode(CORNER)
+        rect(330, 380, 143, 143)
+        imageMode(CORNER)
+        image(images[5], 330, 380, 143, 143)
 
-# Rightbutton
-    fill(0)
-    rectMode(CORNER)
-    rect(600, 500, 120, 50)
+    if mousePressed:
+        if (mouseX > 330 and mouseX < (330 + 143) and mouseY > 380 and mouseY < (380 + 143)):
+            rollRightDice()
+            gedrukt = True
+    

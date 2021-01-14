1d98488f02f746d3a3a4b9e8db4ff71496d54192
Arco Meerkerk <arco-meerkerk@live.nl>
2021-01-14 02:35:58 +0100

    Finsished install

--- setup/ExportProjectFiles.ps1
+++ setup/ExportProjectFiles.ps1
@@ -1,80 +1,82 @@
 param (
     $JavaDir32 = "C:\Program Files (x86)\Java\jre1.8.0_271",
-    $JavaDir64 = "C:\dev\Processing\processing.py-3017-windows64\jre"
+    $JavaDir64 = "C:\dev\Processing\processing.py-3017-windows64\jre",
+    $Launch4jExe = "C:\Program Files (x86)\Launch4j\launch4jc.exe",
+    $InnoSetupExe = "C:\Program Files (x86)\Inno Setup 6\iscc.exe"
 )
 
 Set-Location -Path $PSScriptRoot
 
 function CheckAndMakeDir {
-    param (
-        $Path
-    )
-    
-    if ($Path -is [array]) {
-        Foreach ($p in $Path)
-        {
-            CheckAndMakeDir $p
-        } 
-    }
-    else {
-        if (-not (Test-Path -LiteralPath $Path)) {
-            New-Item -ItemType directory -Path $Path > $null
-            "Created directory" + $Path
-        }
+param (
+    $Path
+)
+
+if ($Path -is [array]) {
+    Foreach ($p in $Path)
+    {
+        CheckAndMakeDir $p
+    } 
+}
+else {
+    if (-not (Test-Path -LiteralPath $Path)) {
+        New-Item -ItemType directory -Path $Path > $null
+        "Created directory" + $Path
     }
 }
+}
 function CheckAndRenameDir {
-    param (
-        $Path,
-        $Name
-        )
-        
-        if (Test-Path -LiteralPath $Path) {
-            Rename-Item -Path $Path -NewName $Name > $null
-            "Renamed " + $Path + " to " + $Name
-    }
+param (
+    $Path,
+    $Name
+    )
+    
+    if (Test-Path -LiteralPath $Path) {
+        Rename-Item -Path $Path -NewName $Name > $null
+        "Renamed " + $Path + " to " + $Name
+}
 }
 
 function CheckAndCopyDir {
-    param (
-        $SourcePath,
-        $TargetPath,
-        $TargetName,
-        $Multiple = $false
-    )
-    $TempCopyFolderOldPath = $TargetPath + '\' + (Split-Path $SourcePath -Leaf)
-    if ($null -ne $TargetName) {
-        $CustomTargetFolderPath = $TargetPath + "\" + $TargetName
-    }
-    else {
-        $CustomTargetFolderPath = $TargetPath
-    }
+param (
+    $SourcePath,
+    $TargetPath,
+    $TargetName,
+    $Multiple = $false
+)
+$TempCopyFolderOldPath = $TargetPath + '\' + (Split-Path $SourcePath -Leaf)
+if ($null -ne $TargetName) {
+    $CustomTargetFolderPath = $TargetPath + "\" + $TargetName
+}
+else {
+    $CustomTargetFolderPath = $TargetPath
+}
 
-    if ($SourcePath -is [array]) {
-        Foreach ($path in $SourcePath)
-        {
-            CheckAndCopyDir $path $TargetPath -Multiple $Multiple
-        } 
+if ($SourcePath -is [array]) {
+    Foreach ($path in $SourcePath)
+    {
+        CheckAndCopyDir $path $TargetPath -Multiple $Multiple
+    } 
+}
+else {
+    if ((Test-Path -LiteralPath $SourcePath) -and
+        (Test-Path -LiteralPath $TargetPath) -and
+        #Check if source directory name doesn't exist in target path
+        -not (Test-Path -LiteralPath $TempCopyFolderOldPath) -and
+        (((Get-ChildItem $TargetPath | Measure-Object).Count -eq 0) -or
+        (
+            #Check if a custom folder is given
+            ($null -ne $TargetName) -and
+            #Check if custom check folder doesn't exists
+            -not (Test-Path -LiteralPath $CustomTargetFolderPath)
+        ) -or
+        ($Multiple -eq $true))
+    ) {
+        "Copying " + $SourcePath
+        Copy-Item -Path $SourcePath -Destination $CustomTargetFolderPath -Recurse
+        "Copied to folder " + $TempCopyFolderOldPath
     }
-    else {
-        if ((Test-Path -LiteralPath $SourcePath) -and
-            (Test-Path -LiteralPath $TargetPath) -and
-            #Check if source directory name doesn't exist in target path
-            -not (Test-Path -LiteralPath $TempCopyFolderOldPath) -and
-            (((Get-ChildItem $TargetPath | Measure-Object).Count -eq 0) -or
-            (
-                #Check if a custom folder is given
-                ($null -ne $TargetName) -and
-                #Check if custom check folder doesn't exists
-                -not (Test-Path -LiteralPath $CustomTargetFolderPath)
-            ) -or
-            ($Multiple -eq $true))
-        ) {
-            "Copying " + $SourcePath
-            Copy-Item -Path $SourcePath -Destination $CustomTargetFolderPath -Recurse
-            "Copied to folder " + $TempCopyFolderOldPath
-        }
-    }
+}
 }
 
 function CheckAndCopyFile {
@@ -98,26 +100,33 @@ function CheckAndCopyFile {
 
 
 function ExportProject {
-    param (
-        $exportFolder,
-        $JavaDir
-    )
+param (
+    $exportFolder,
+    $JavaDir
+)
     $libPath1 = '..\application.windows32\lib'
     $libPath2 = '..\application.windows64\lib'
     $processingFolders = '..\modules', '..\assets'
     $processingFiles = '..\PadNaarMysterieExtension.pyde'
-    $processingExportFolder = $exportFolder + '\processing'
 
-    CheckAndMakeDir $exportFolder, $processingExportFolder
-    
+    CheckAndMakeDir $exportFolder
+
     CheckAndCopyDir $libPath1 $exportFolder "libraries"
     CheckAndCopyDir $libPath2 $exportFolder "libraries"
     CheckAndCopyDir $JavaDir $exportFolder "jre"
-    CheckAndCopyDir $processingFolders $processingExportFolder -Multiple $true
-    CheckAndCopyFile $processingFiles $processingExportFolder
+    CheckAndCopyDir $processingFolders $exportFolder -Multiple $true
+    CheckAndCopyFile $processingFiles $exportFolder
 }
 
 
 $exportFolderPath = '.\bin\export'
 ExportProject ($exportFolderPath + '32') $JavaDir32
 ExportProject ($exportFolderPath + '64') $JavaDir64
+
+
+# iscc setup.iss /DMyAppPlatform=32
+ 
+Start-Process -NoNewWindow -Wait -FilePath $Launch4jExe -ArgumentList 'build32.xml'
+Start-Process -NoNewWindow -Wait -FilePath $Launch4jExe -ArgumentList 'build64.xml'
+Start-Process -NoNewWindow -Wait -FilePath $InnoSetupExe -ArgumentList 'setup.iss',"/DMyAppPlatform=32"
+Start-Process -NoNewWindow -Wait -FilePath $InnoSetupExe -ArgumentList 'setup.iss',"/DMyAppPlatform=64"
